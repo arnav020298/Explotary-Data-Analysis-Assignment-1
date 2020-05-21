26ab0452e8874ed78adbb0c73b9d54b9be27da42
arnav020298 <63341632+arnav020298@users.noreply.github.com>
2020-05-21 17:46:45 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -29,7 +29,7 @@ plot3 <- function(){
         subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
         
         ## plot globalactivepower vs date&time
-        png("plot3.png", width=480, height=480)
+        png("plot3.PNG", width=480, height=480)
         with(subsetdata, plot(FullTimeDate, Sub_metering_1, type="l", xlab="Day", ylab="Energy sub metering"))
         lines(subsetdata$FullTimeDate, subsetdata$Sub_metering_2,type="l", col= "red")
         lines(subsetdata$FullTimeDate, subsetdata$Sub_metering_3,type="l", col= "blue")
